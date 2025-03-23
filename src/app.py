98992a46dcabf95dda092c6ae789448ee8211bb8
Gildas Montcho <gmontcho@gmail.com>
2025-03-23 23:51:18 +0100

    feat(src/app.py): ajout routes GET et POST pour "/todos"

--- src/app.py
+++ src/app.py
@@ -1,14 +1,37 @@
-from flask import Flask
+from flask import Flask, request
 
 from src.constants import HEALTH_MSG
 
+app = Flask(__name__)
 
-def create_app(config):
-    app = Flask(__name__)
-    app.config.from_object(config)
+todos = [
+    {"title": "Coder l'application", "description": "Todo list", "completed": False}
+]
 
-    @app.route("/health")
-    def api_health():
-        return HEALTH_MSG
 
-    return app
+@app.get("/todos")
+def get_todos():
+    return {"todos": todos}
+
+
+@app.post("/todos")
+def create_todo():
+    request_data = request.get_json()
+    new_todo = {
+        "title": request_data["title"],
+        "description": request_data["description"],
+        "completed": False,
+    }
+    todos.append(new_todo)
+    return new_todo, 201
+
+
+# def create_app(config):
+#     app = Flask(__name__)
+#     app.config.from_object(config)
+#
+#     @app.route("/health")
+#     def api_health():
+#         return HEALTH_MSG
+#
+#     return app
